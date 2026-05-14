91676f56ddafa566af5b23d2423d01ba1f17b87b
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-05-14 13:36:59 +0300

    feat: convert mf4 to pandas dataframe

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
+from asammdf import MDF
 
+mdf = MDF("sample.mf4")
+
+df = mdf.to_dataframe(
+    channels=["U", "I"],
+    raster="U",
+    time_from_zero=True,
+)
+
+df = df.cumsum()
+df.plot()
+plt.show()
