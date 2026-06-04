63b98c427c36e0b5e4856e7c0d6c1b99c7023b78
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-06-04 19:20:14 +0300

    WIP: mcu dataset init

--- src/mcu_sample.py
+++ src/mcu_sample.py
@@ -4,17 +4,20 @@ from asammdf import MDF
 
 
 class McuSample:
-    def __init__(self, filepath: Path, raster_freq: float, qnom: int):
+    def __init__(self, filepath: Path, raster: float, qnom: int):
         self.filepath = filepath
         mdf = MDF(filepath)
-        signal = mdf.get(name="U", raster=raster_freq)
-        qneg = mdf.get(name="Qneg", raster=raster_freq)
+        signal = mdf.get(name="U", raster=raster)
+        qneg = mdf.get(name="Qneg", raster=raster)
 
         self.n_samples = len(signal)
-        self.path = filepath
+        self.filepath = filepath
         self.soh = abs(float(qneg.samples.min())) / qnom * 100
 
         mdf.close()
 
     def __len__(self):
         return self.n_samples
+
+    def load_window(self, start: int, end: int):
+        print("WIP")
