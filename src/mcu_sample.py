a0cf80ecf800bae6328d5a5aa2142ec223f30b01
yousefalfoqaha <yousefalfoqaha@proton.me>
2026-06-05 10:37:45 +0300

    hdf5 conversion test

--- src/mcu_sample.py
+++ src/mcu_sample.py
@@ -2,14 +2,23 @@ from pathlib import Path
 
 from asammdf import MDF
 
+TARGET_CHANNELS = ["U", "I", "Qneg", "Temp[1]"]
+
 
 class McuSample:
     def __init__(self, filepath: Path, raster: float, qnom: int):
-        mdf = MDF(filepath)
+        mdf = MDF(name=filepath, channels=TARGET_CHANNELS)
 
         hdf5_path = filepath.with_suffix(".hd5")
         if not hdf5_path.exists():
-            mdf.export(fmt="hdf5", filename=hdf5_path)
+            mdf.export(
+                fmt="hdf5",
+                filename=hdf5_path,
+                single_time_base=True,
+                raster=raster,
+            )
+
+        mdf.close()
 
         signal = mdf.get(name="U", raster=raster)
         qneg = mdf.get(name="Qneg", raster=raster)
@@ -17,8 +26,6 @@ class McuSample:
         self.filepath = hdf5_path
         self.n_samples = len(signal)
         self.soh = abs(float(qneg.samples.min())) / qnom * 100
-
-        mdf.close()
 
     def __len__(self):
         return self.n_samples
